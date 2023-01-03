0610add0b876ccd7c0529ec8134fa86c5bfb2797
Seanonline <l1f07bscs0035@gmail.com>
2023-01-03 04:46:03 -0500

    base-line of the web-api project.

---
+++ asp.net-core-6-jwt-authentication/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace asp.net_core_6_jwt_authentication
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
